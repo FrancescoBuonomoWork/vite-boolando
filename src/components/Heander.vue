7397939235c4e121a36bd4f5fef3d96ab62bde39
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-06 16:03:24 +0100

    add variables

--- src/components/Heander.vue
+++ src/components/Heander.vue
@@ -10,5 +10,13 @@ export default {
 
 <style scoped lang="scss">
 // @use './styles/general.scss';
+@use '../styles/partials/variables';
+.header-page {
+    background-color: $header-color;
+    height: 10vh;
+    width: 100%;
+    position: fixed;
+    z-index: 1;
+}
 
 </style>
