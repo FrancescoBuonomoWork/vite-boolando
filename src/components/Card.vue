204249d33ad83a98365700109bbdbd776dbbb1cd
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-09 18:30:24 +0100

    fix

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,7 @@
 <script>
 export default {
     name: 'Card',
+    emits: ['show'],
     props: {
         product: {
             type: Object
--- src/components/HeanderPage.vue
+++ src/components/HeanderPage.vue
@@ -2,6 +2,7 @@
 
 export default { 
     name: 'HeaderPage',
+    
     components:{
     },
     data() {
